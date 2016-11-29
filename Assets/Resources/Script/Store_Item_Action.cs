6acf7de8338ac5fa76fd78c257780a1550cbfd27
JeongSuhong <ghjkdfe@gmail.com>
2016-11-29 17:47:16 +0900

    1129. 상점 UI 만드는중

---
+++ Assets/Resources/Script/Store_Item_Action.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Store_Item_Action : MonoBehaviour {
+
+    public Camera ItemOBJ_Camera;
+    public Transform ItemOBJ_Position;
+    GameObject ItemOBJ;
+
+	void Start()
+    {
+        ItemOBJ = transform.FindChild("ItemModel").gameObject;
+    }
+    
+    // Update is called once per frame
+	void Update () {
+
+       Vector3 pos = ItemOBJ_Camera.ScreenToViewportPoint(ItemOBJ_Position.position);
+
+        pos.z = 0f;
+
+        ItemOBJ.transform.position = pos;
+
+        ItemOBJ.transform.GetChild(0).Rotate(Vector3.up * Time.deltaTime * 10f);
+	}
+}
